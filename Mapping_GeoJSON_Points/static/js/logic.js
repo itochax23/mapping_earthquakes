928f18509f046b7562688383c12a8b248dd247f9
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-10-03 18:26:26 -0500

    Add GeoJSON Point

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -25,14 +25,23 @@ let sanFranAirport = {
 ]};
 
 // Grabbing our GeoJSON data.
-L.geoJSON(sanFranAirport, {
-  // Turn each feature into a marker on the map.
-  pointToLayer: function(feature, latlng) {
-    console.log(feature);
-    return L.marker(latlng);
+L.geoJson(sanFranAirport, {
+  onEachFeature: function(feature, layer) {
+    console.log(layer);
+    layer.bindPopup("<h2>Airport Code: " + feature.properties.faa + "</h2> <hr>" + "<h3>Airport name: " + feature.properties.name + "</h3>");
   }
-  
 }).addTo(map);
+
+
+// L.geoJSON(sanFranAirport, {
+//   // Turn each feature into a marker on the map.
+//   pointToLayer: function(feature, latlng) {
+//     console.log(feature);
+//     return L.marker(latlng)
+//     .bindPopup("<h2>" + feature.properties.name + "</h2> <hr>" + "<h3>" + feature.properties.city + ", " + feature.properties.country + "</h3>");
+//   }
+
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
@@ -51,5 +60,6 @@ mapbox/outdoors-v11
 mapbox/light-v10
 mapbox/dark-v10
 mapbox/satellite-v9
-mapbox/satellite-streets-v11 */
+mapbox/satellite-streets-v11 
+mapbox://styles/mapbox/navigation-night-v1*/
 
