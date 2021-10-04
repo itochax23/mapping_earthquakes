fed4a79f7b9cf5e9094a71491808bce7c16499dd
itochax23 <86204563+itochax23@users.noreply.github.com>
2021-10-03 20:57:58 -0500

    Add legend

--- Earthquakes_past7days/static/js/logicStep5.js
+++ Earthquakes_past7days/static/js/logicStep5.js
@@ -72,6 +72,36 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geoj
     weight: 0.5
   };
 }
+
+// Create a legend control object.
+let legend = L.control({
+    position: "bottomright"
+  });
+  // Then add all the details for the legend.
+legend.onAdd = function() {
+    let div = L.DomUtil.create("div", "info legend");
+    const magnitudes = [0, 1, 2, 3, 4, 5];
+    const colors = [
+      "#98ee00",
+      "#d4ee00",
+      "#eecc00",
+      "#ee9c00",
+      "#ea822c",
+      "#ea2c2c"
+    ];
+    // Looping through our intervals to generate a label with a colored square for each interval.
+    for (var i = 0; i < magnitudes.length; i++) {
+        console.log(colors[i]);
+        div.innerHTML +=
+            "<i style='background: " + colors[i] + "'></i> " +
+            magnitudes[i] + (magnitudes[i + 1] ? "&ndash;" + magnitudes[i + 1] + "<br>" : "+");
+    }
+    return div;
+    };
+
+    legend.addTo(map);
+});
+
 // This function determines the color of the circle based on the magnitude of the earthquake.
 function getColor(magnitude) {
     if (magnitude > 5) {
@@ -97,7 +127,6 @@ function getColor(magnitude) {
     }
     return magnitude * 4;
     };
-});
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps, overlays).addTo(map);
